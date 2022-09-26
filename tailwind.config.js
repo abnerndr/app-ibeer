67b23c343b6f71218f74a3c9fea086cb778d0e7b
Abner André <abner@oialbert.com.br>
2022-09-25 23:52:47 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'ibeer-900': '#FFBD4A',
+        'ibeer-800': '#FFCA28'
+      }
+    },
+  },
+  plugins: [],
+}
