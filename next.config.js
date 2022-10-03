c46deca6f77c251946a84a83c788d5b30383b690
Abner André <abner@oialbert.com.br>
2022-10-03 13:50:22 -0300

    feat: instalado o capacitor e feito configurações extras

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
-
-// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
-if (process.env.NODE_ENV === 'development') {
-  console.log('info  - developUrl:', `http://${require('address').ip()}:3000`)
-}
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    baseURL: process.env.API_URL ,
-  },
-  eslint: {
-    dirs: ['src']
-  },
-  typescript: { ignoreBuildErrors: true },
- 
+  swcMinify: true,
 }
 
 module.exports = nextConfig
+// /** @type {import('next').NextConfig} */
+// if (process.env.NODE_ENV === 'development') {
+//   console.log('info  - developUrl:', `http://${require('address').ip()}:3000`)
+// }
+// const nextConfig = {
+//   reactStrictMode: true,
+//   env: {
+//     baseURL: process.env.API_URL ,
+//   },
+//   eslint: {
+//     dirs: ['src']
+//   },
+//   typescript: { ignoreBuildErrors: true },
+ 
+// }
+
+// module.exports = nextConfig
